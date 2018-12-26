1bbfb05f3039205e05d6d7a0e09c2bf4cd8a652a
Mohammed alJaser <aljaserm7@gmail.com>
2018-12-26 08:41:31 -0500

    Started with the product type views in the admin area

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes value)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(value);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(value);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.productTypes.ToList());
+        }
+    }
+}
