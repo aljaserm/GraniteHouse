cf671d5f1433d2847b482e150c3652ec1d8806ae
Mohammed alJaser <aljaserm7@gmail.com>
2019-01-05 13:32:16 -0500

    Completed Product Section

---
+++ GraniteHouse/Extensions/IEnumerableExt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Extensions
+{
+    public static class IEnumerableExt
+    {
+        public static IEnumerable<SelectListItem> selectListItems<T>(this IEnumerable<T> items,int selectedVal)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetProperyValue("Name"),
+                       Value = item.GetProperyValue("Id"),
+                       Selected = item.GetProperyValue("Id").Equals(selectedVal.ToString())
+                   };
+        }
+    }
+}
